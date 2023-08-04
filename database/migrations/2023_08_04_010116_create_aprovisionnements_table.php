8f914a8710fda6ecb1e2d228c59c8002ff362852
coder <sowpharell98@hotmail.com>
2023-08-04 01:15:13 +0000

    first commit3

---
+++ database/migrations/2023_08_04_010116_create_aprovisionnements_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aprovisionnements', function (Blueprint $table) {
+            $table->id("id_approvisionnement");
+            $table->dateTime("dateApprovisionnement");
+            $table->dateTime("dateExpiration");
+            $table->int("quantite");
+            $table->float("pu");
+            $table->float("prix");
+            $table->foreignId('id_produit')->references("id_produit")->on("produits");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aprovisionnements');
+    }
+};
